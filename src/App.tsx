2c81edf40265293fd855102bba57fcd5ab44e023
oshanadi <oshanweerasinghe28@gmail.com>
2025-03-13 19:06:34 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <img src="/logo.png" alt="Ceyplanta Logo" className="logo" />
+        <h1 className="title">🌱 Ceyplanta - Coming Soon</h1>
+        <p className="text">We're working on something amazing! Stay tuned.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
