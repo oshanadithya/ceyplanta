094b6113e896fa19ac27063cb7f23e1822dcd549
oshanadi <oshanweerasinghe28@gmail.com>
2025-11-04 09:07:08 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import './Footer.css'; // You can create a separate CSS file for the footer
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>©️ 2025 Ceyplanta. All Rights Reserved.</p>
+      <p>©️ 2024 Ceyplanta. All Rights Reserved. Designed & Developed by SriFUTURE</p>
     </footer>
   );
 };
