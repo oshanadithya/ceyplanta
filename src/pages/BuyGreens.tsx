b72e27f7d3b3f2eda996cf016dc8f280004e0799
oshanadi <oshanweerasinghe28@gmail.com>
2025-12-26 17:47:26 +0530

    Update BuyGreens.tsx

--- src/pages/BuyGreens.tsx
+++ src/pages/BuyGreens.tsx
@@ -148,7 +148,7 @@ const BuyGreens = () => {
               { weight: "100g", price: "Rs. 860" },
               { weight: "200g", price: "Rs. 1620" },
             ],
-            noStock: true,
+            noStock: false,
             category: "Microgreens",
           },
           {
